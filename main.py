46cdcabfe75f23a8ed2110d22a38d6e5cfc924bc
samy007 <samy2014@free.fr>
2025-02-15 16:35:47 +0100

    feat: completed the paper's algorithms

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 chars = "0123456789ABCDEF"
+# https://inria.hal.science/file/index/docid/864293/filename/get_str.pdf
 
 
 def naive(n, b):
@@ -11,6 +13,8 @@ def naive(n, b):
 
 
 def div_free_naive(a, b, k):
+    assert a < b**k
+
     def convert_naive(y, k, n):
         string = ""
         for i in range(1, k + 1):
@@ -22,3 +26,82 @@ def div_free_naive(a, b, k):
     y = ((a + 1) * (2**n)) // (b**k) - 1
     return convert_naive(y, k, n)
 
+
+def div_free_naive_trunc(a, b, k):
+    assert a < b**k
+
+    def convert_trunc(y, k, n):
+        alpha = math.log2(b)
+        string = ""
+        for i in range(1, k + 1):
+            ni = n - math.floor(i * alpha)
+            ni_1 = n - math.floor((i-1) * alpha)
+            t = b * y
+            string += chars[t // (2**ni_1)]
+            z = t % 2**ni_1
+            y = z >> (ni_1 - ni)
+        return string
+
+    n = (2 * k * (b**k)).bit_length()
+    y = ((a+1) * (2**n)) // (b**k) - 1
+    return convert_trunc(y, k, n)
+
+
+def subquadratic_div_free(a, b, kt):
+    assert kt > 2
+
+    def convert_trunc(y, k, n):
+        # print("trunc", y, k, n)
+        alpha = math.log2(b)
+        # string = ""
+        string = []
+        for i in range(1, k + 1):
+            ni = n - math.floor(i * alpha)
+            ni_1 = n - math.floor((i-1) * alpha)
+            t = b * y
+            # string += chars[t // (2**ni_1)]
+            string.append(t // (2**ni_1))
+            z = t % 2**ni_1
+            y = z >> (ni_1 - ni)
+        return string
+
+    def convert_rec(a, k, y, n, g):
+        # print("rec", a, k, y, n, g)
+        if k <= kt:
+            return convert_trunc(y, k, n)
+
+        kh = (k + 1) // 2
+        kl = k - kh + 1
+        nh = (4 * g * (b ** kh)).bit_length()
+        nl = (4 * g * (b ** kl)).bit_length()
+        # ah = math.floor(a * b**(kh - k))
+        # al = a % (b**kl)
+        ah = al = 0
+        yh = math.floor(y * 2**(nh-n))
+        yl = ((y*b**(k-kl)) % (2**n)) >> (n-nl)
+        sh = convert_rec(ah, kh, yh, nh, g)
+        sl = convert_rec(al, kl, yl, nl, g)
+
+        if sh[-1] == b-1 and sl[0] == 0:
+            print("a")
+            # add 1 mod b^kh
+            carry = 1
+            for i in range(len(sh) - 1, -1, -1):
+                s = sh[i] + carry
+                sh[i] = s % b
+                carry = s // b
+        if sh[-1] == 0 and sl[0] == b-1:
+            sl = [0] * kl
+        return sh[0:-1] + sl
+
+    # if a is a power of b, b ** k == a, but we want a < b ** k
+    # ceil is required because of float imprecision
+    k = math.ceil(math.log(a, b)) + 1
+    g = max(math.ceil(math.log2(k)) + 1, kt)
+    # g = max(kt, math.ceil(math.log2((k - 3) / (kt - 3))))
+    n = (4 * g * b**k).bit_length()
+    y = ((a+1) * 2**n) // (b**k) - 1
+    return "".join(map(lambda x: chars[x], convert_rec(a, k, y, n, g)))
+
+
+print(subquadratic_div_free(1000, 10, 4))
